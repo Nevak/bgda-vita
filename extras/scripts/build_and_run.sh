2635da45ad68213f01c9c6daa3fe71f6cc09cb9f
RaulLpg <raul@lanpartygames.com>
2025-03-02 17:07:05 +0800

    Enabled SAFE_UNIFORMS for vitaGL in build script

--- extras/scripts/build_and_run.sh
+++ extras/scripts/build_and_run.sh
@@ -1,6 +1,6 @@
 cd /vita-dev/vitaGL 
 make clean
-make SOFTFP_ABI=1 LOG_ERRORS=1 HAVE_GLSL_SUPPORT=0 HAVE_SHARK_LOG=1 install 
+make SOFTFP_ABI=1 LOG_ERRORS=1 HAVE_GLSL_SUPPORT=0 HAVE_SHARK_LOG=1 NO_DEBUG=1 SAFE_UNIFORMS=1 install 
 cd /vita-dev/soulcalibur_vita/build 
 cmake .. -DCMAKE_BUILD_TYPE=Debug 
 make clean
