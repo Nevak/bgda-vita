665db4bd763d4f37cdc92e25f93433309708854c
Nevak <>
2026-01-17 21:04:30 +0800

    Enabled x2 AA as it doesn't seem to affect performance

--- source/utils/glutil.c
+++ source/utils/glutil.c
@@ -54,7 +54,7 @@
     //vglInitWithCustomThreshold(0, 960, 544, 18 * 1024 * 1024, 0, 20 * 1024 * 1024, 0, SCE_GXM_MULTISAMPLE_NONE);
 
     
-    vglInitWithCustomThreshold(0, SCREEN_W, SCREEN_H, MEMORY_VITAGL_THRESHOLD_MB * 1024 * 1024, 0, 0, 0, SCE_GXM_MULTISAMPLE_NONE);
+    vglInitWithCustomThreshold(0, SCREEN_W, SCREEN_H, MEMORY_VITAGL_THRESHOLD_MB * 1024 * 1024, 0, 0, 0, SCE_GXM_MULTISAMPLE_2X);
     //eglSwapInterval(0, 2);
  }
  
